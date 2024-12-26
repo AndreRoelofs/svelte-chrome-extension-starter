68515d02e9375d72c8c483922c9e26849b0fc177
Andre Roelofs <andreroelofsai@gmail.com>
2024-12-26 11:59:57 +0100

    feat: completed the content script side of the extension

--- apps/scripts/content/src/lib/content.ts
+++ apps/scripts/content/src/lib/content.ts
@@ -1,34 +1,31 @@
-// import TestButton from '$lib/components/TestButton.svelte';
-// import { Button } from '$lib/mountable-components/button/index.js';
-// import '$lib/mountable-components/button/index.ts';
-// import YoutubePlayerCreator from 'youtube-player';
-// import { YouTubePlayer } from 'youtube-player/dist/types.js';
-// import { mountIfExists } from '@svelte-chrome-extension-starter/utils';
-// import '$custom-elements/bookmark-button';
-// import { BookmarkButton } from '$custom-elements/bookmark-button/src/index.js';
 import '@webcomponents/custom-elements';
 import '$custom-elements/bookmark-button/dist/main.js';
-// import * as bookmarkStyle from '$custom-elements/bookmark-button/style.css?inline';
-// import '$custom-elements/bookmark-button/Button.css';
 
 (() => {
     let youtubeLeftControls: HTMLElement;
     // let youtubePlayer: YouTubePlayer;
-    // let currentVideo = '';
-    // let currentVideoBookmarks = [];
+    let youtubePlayer: HTMLVideoElement;
+    let currentVideo = '';
+    let currentVideoBookmarks = [];
 
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        // const { type, value, videoId } = obj;
-        const { type } = obj;
+    chrome.runtime.onMessage.addListener((obj, _, response) => {
+        const { type, value, videoId } = obj;
 
         if (type === 'NEW') {
-            // currentVideo = videoId;
+            currentVideo = videoId;
+            console.log(currentVideo);
             newVideoLoaded();
         } else if (type === 'PLAY') {
-            // console.log('PLAY', obj);
-            // if (youtubePlayer) {
-            //     youtubePlayer.seekTo(5, true);
-            // }
+            youtubePlayer.currentTime = value;
+        } else if (type === 'DELETE') {
+            currentVideoBookmarks = currentVideoBookmarks.filter(
+                (b) => b.time != value,
+            );
+            chrome.storage.sync.set({
+                [currentVideo]: JSON.stringify(currentVideoBookmarks),
+            });
+
+            response(currentVideoBookmarks);
         }
     });
 
@@ -39,84 +36,56 @@ import '$custom-elements/bookmark-button/dist/main.js';
             '.ytp-left-controls',
         ) as HTMLElement;
 
-        // youtubePlayer = document.getElementsByClassName('video-stream')[0];
-        // youtubePlayer = YoutubePlayerCreator.default('video-player');
+        youtubePlayer = document.getElementsByClassName(
+            'video-stream',
+        )[0] as HTMLVideoElement;
 
         if (!youtubeLeftControls) {
             console.error('Could not find youtubeLeftControls');
             // return;
         }
-        // Create a div and prepend it to the body
-        // const div = document.createElement('div');
-        // div.id = 'test-button-container';
-        // document.body.prepend(div);
 
-        // mountIfExists(TestButton, {
-        //     target: youtubeLeftControls,
-        //     props: {
-        //         text: 'world',
-        //     },
-        // });
-
-        // mountIfExists(Button, {
-        //     target: youtubeLeftControls,
-        //     props: {
-        //         text: 'Click Me!',
-        //     },
-        // });
-
-        // customElements.define(
-        //     'tw-bookmark-button',
-        //     Button as unknown as CustomElementConstructor,
-        // );
-        // ===============================
         const button = document.createElement('tws-bookmark-button');
         console.log('Button created', button);
         button['text'] = 'Bookmark';
 
-        // button.id = 'my-button';
-
-        // Make the position absolute and put in the middle of the screen
-        // button.style.position = 'absolute';
-        // button.style.top = '50%';
-        // button.style.left = '50%';
-
-        // Give z-index so it's on top of everything
-        // button.style.zIndex = '9999';
-
-        // const sheet = new CSSStyleSheet();
-        // sheet.replaceSync(bookmarkStyle.default);
-
-        // button.shadowRoot.adoptedStyleSheets = [sheet];
-
         youtubeLeftControls.append(button);
 
-        // const div = document.createElement('div');
-        // div.appendChild(button);
-
-        // document.body.append(div);
-
-        // ===============================
-
-        // const app = new TestButton({
-        //     target: div,
-        //     props: {
-        //         text: 'world',
-        //     },
-        // });
-
-        // Create a new button and prepend it to the body
+        button.addEventListener('click', addNewBookmarkEventHandler);
     };
 
-    // const container = document.querySelector('ytd-watch-metadata')!;
-    // container.innerHTML = '<h1>Test Svelte App</h1>';
-    // document.body.prepend(container);
+    const fetchBookmarks = () => {
+        return new Promise<[]>((resolve) => {
+            chrome.storage.sync.get([currentVideo], (obj) => {
+                resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+            });
+        });
+    };
 
-    // function content(): string {
-    //     return 'content Svelte app';
-    // }
+    const getTime = (t) => {
+        const date = new Date(0);
+        date.setSeconds(t);
 
-    // console.log(content());
+        return date.toISOString().substring(11, 8);
+    };
+
+    const addNewBookmarkEventHandler = async () => {
+        const currentTime = youtubePlayer.currentTime;
+        const newBookmark = {
+            time: currentTime,
+            desc: 'Bookmark at ' + getTime(currentTime),
+        };
+
+        currentVideoBookmarks = await fetchBookmarks();
+
+        chrome.storage.sync.set({
+            [currentVideo]: JSON.stringify(
+                [...currentVideoBookmarks, newBookmark].sort(
+                    (a, b) => a.time - b.time,
+                ),
+            ),
+        });
+    };
 
     newVideoLoaded();
 })();
