ac1b5c7718f006b46fff145f7af9369c4ab24896
Andre Roelofs <andreroelofsai@gmail.com>
2024-12-26 11:37:50 +0100

    feat: adds a ladder of postcss config inheritance for avoiding custom postcss plugin code repetition

--- libs/custom-elements/bookmark-button/postcss.config.cjs
+++ libs/custom-elements/bookmark-button/postcss.config.cjs
@@ -1,20 +1,11 @@
-const convertRemToEm = {
-    postcssPlugin: 'convertRemToEm',
-    // When debugging this, https://astexplorer.net/#/2uBU1BLuJ1 is very helpful
-    Declaration(declaration) {
-        declaration.value = declaration.value.replaceAll(remRegex, 'em');
-    },
-};
-
-// Regex to find all occurrences of "rem" units
-const remRegex = /(?<=\d)rem/g;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const parentConfig = require('../postcss.config.cjs');
 
 module.exports = {
+    ...parentConfig,
     plugins: [
-        require('postcss-import'),
+        ...parentConfig.plugins,
         // eslint-disable-next-line @typescript-eslint/no-var-requires
         require('tailwindcss')('./tailwind.config.cjs'),
-        require('autoprefixer'),
-        convertRemToEm,
     ],
 };
