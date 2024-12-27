d9b92ac47d7b3109d8e1d57c39069cab084591f4
Andre Roelofs <andreroelofsai@gmail.com>
2024-12-27 19:22:46 +0100

    fixed an issue with background script listener triggering multiple times

--- apps/scripts/background/src/lib/background.ts
+++ apps/scripts/background/src/lib/background.ts
@@ -1,5 +1,9 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (tab.url && tab.url.includes('youtube.com/watch')) {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (
+        changeInfo.status === 'complete' &&
+        tab.url &&
+        tab.url.includes('youtube.com/watch')
+    ) {
         const queryParameters = tab.url.split('?')[1];
         const urlParameters = new URLSearchParams(queryParameters);
 
@@ -11,23 +15,3 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         });
     }
 });
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//     // Only run when the tab is done loading,
-//     // and make sure it's the right YouTube watch page
-//     if (
-//         changeInfo.status === 'complete' &&
-//         tab.url &&
-//         tab.url.includes('youtube.com/watch')
-//     ) {
-//         // Instead of splitting off only the first parameter,
-//         // pass the full query string to URLSearchParams
-//         const video = tab.url.split('v=')[1].split('&')[0];
-//         // const urlParameters = new URLSearchParams(queryString);
-
-//         chrome.tabs.sendMessage(tabId, {
-//             type: 'NEW',
-//             videoId: urlParameters.get('v'),
-//         });
-//     }
-// });
