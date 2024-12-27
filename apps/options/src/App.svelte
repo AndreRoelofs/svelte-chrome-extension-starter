c176b2378aac511a93fae2942b8ecd9d9b7eb2ba
Andre Roelofs <andreroelofsai@gmail.com>
2024-12-27 19:39:02 +0100

    feat: option page now displays all your bookmarks across all videos

--- apps/options/src/App.svelte
+++ apps/options/src/App.svelte
@@ -1,75 +1,141 @@
 <script>
+import * as utils from '@svelte-chrome-extension-starter/utils';
 import { onMount } from 'svelte';
-import * as RadioGroup from '$lib/components/radio-group/index.js';
-import { Label } from '$lib/components/label/index.js';
+import * as Table from '$lib/components/table';
+import { Button } from '$lib/components/button';
+import Play from 'lucide-svelte/icons/play';
+import Trash from 'lucide-svelte/icons/trash';
 
-// Default options
-let options = {
-    sortOrder: 'date', // or 'alphabetical'
+let bookmarks = [];
+
+const fetchBookmarks = async () => {
+    const keys = await chrome.storage.sync.getKeys();
+    console.log(keys);
+
+    const newBookmarks = [];
+
+    const data = await chrome.storage.sync.get(keys);
+    for (const key in data) {
+        const bookmarkObject = JSON.parse(data[key]);
+        newBookmarks.push({
+            name: bookmarkObject[0].title,
+            items: [...bookmarkObject],
+        });
+    }
+
+    bookmarks = newBookmarks;
+
+    console.log('pop-up bookmarks', bookmarks);
+};
+const onPlay = async (timestamp) => {
+    const activeTab = await utils.getActiveTabURL();
+
+    chrome.tabs.sendMessage(activeTab.id, {
+        type: 'PLAY',
+        value: timestamp,
+    });
 };
 
-// Load current options from chrome.storage
-onMount(async () => {
-    const stored = await chrome.storage.local.get('options');
-    if (stored.options) {
-        options = stored.options;
-    }
-});
+const onDelete = async (timestamp) => {
+    const activeTab = await utils.getActiveTabURL();
 
-// Save options back to chrome.storage
-async function saveOptions() {
-    await chrome.storage.local.set({ options });
-}
+    bookmarks = bookmarks.filter(
+        (bookmark) => bookmark.timestamp !== timestamp,
+    );
 
-// Update the sort order on user interaction
-function setSortOrder(order) {
-    options.sortOrder = order;
-    saveOptions();
-}
+    chrome.tabs.sendMessage(activeTab.id, {
+        type: 'DELETE',
+        value: timestamp,
+    });
+};
+
+const getTime = (t) => {
+    const date = new Date(0);
+    date.setSeconds(t);
+
+    return date.toISOString().substring(11, 19);
+};
+
+const getDateWithTime = (t) => {
+    const date = new Date(t);
+
+    const options = {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+    };
+    const formattedDate = date.toLocaleString('en-US', options);
+
+    return formattedDate;
+};
+
+onMount(fetchBookmarks);
 </script>
 
-<main>
-    <RadioGroup.Root value="comfortable">
-        <div class="flex items-center space-x-2">
-            <RadioGroup.Item value="default" id="r1" />
-            <Label for="r1">Default</Label>
-        </div>
-        <div class="flex items-center space-x-2">
-            <RadioGroup.Item value="comfortable" id="r2" />
-            <Label for="r2">Comfortable</Label>
-        </div>
-        <div class="flex items-center space-x-2">
-            <RadioGroup.Item value="compact" id="r3" />
-            <Label for="r3">Compact</Label>
-        </div>
-    </RadioGroup.Root>
-    <!-- <div class="space-y-4 p-4 text-gray-900 dark:text-white">
-        <h1 class="text-xl font-semibold">Extension Options</h1>
+<main class="bg-background">
+    <article class="prose lg:prose-xl">
+        <h1>The Bookmarker</h1>
+        <p>All your bookmarks</p>
 
-        <div>
-            <label class="mb-2 block font-medium">Snippet Sort Order:</label>
-            <div class="flex items-center gap-4">
-                <label class="flex items-center gap-1">
-                    <input
-                        type="radio"
-                        name="sortOrder"
-                        value="date"
-                        checked={options.sortOrder === 'date'}
-                        on:change={() => setSortOrder('date')}
-                    />
-                    By Date
-                </label>
-                <label class="flex items-center gap-1">
-                    <input
-                        type="radio"
-                        name="sortOrder"
-                        value="alphabetical"
-                        checked={options.sortOrder === 'alphabetical'}
-                        on:change={() => setSortOrder('alphabetical')}
-                    />
-                    Alphabetical
-                </label>
-            </div>
-        </div>
-    </div> -->
+        {#if bookmarks.length > 0}
+            {#each bookmarks as { name, items }}
+                <Table.Root>
+                    <Table.Caption>{name}</Table.Caption>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.Head class="w-[100px]">Timestamp</Table.Head>
+                            <Table.Head class="w-[250px] text-center"
+                                >Created At</Table.Head
+                            >
+                            <Table.Head class="text-right">Actions</Table.Head>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {#each items as { timestamp, title, createdAt }}
+                            <Table.Row>
+                                <Table.Cell>{getTime(timestamp)}</Table.Cell>
+                                <Table.Cell
+                                    >{getDateWithTime(createdAt)}</Table.Cell
+                                >
+                                <Table.Cell class="text-right">
+                                    <Button
+                                        size="icon"
+                                        onclick={onPlay.bind(null, timestamp)}
+                                        ><Play /></Button
+                                    >
+
+                                    <Button
+                                        size="icon"
+                                        onclick={onDelete.bind(null, timestamp)}
+                                        ><Trash /></Button
+                                    >
+                                </Table.Cell>
+                            </Table.Row>
+                        {/each}
+                    </Table.Body>
+                </Table.Root>
+                <br />
+            {/each}
+        {:else}
+            <p>No bookmarks to show.</p>
+        {/if}
+    </article>
 </main>
+
+<style lang="postcss">
+main {
+    text-align: center;
+    padding: 1em;
+    max-width: 700px;
+    margin: 0 auto;
+    width: 500px;
+}
+
+@media (min-width: 640px) {
+    main {
+        max-width: none;
+    }
+}
+</style>
