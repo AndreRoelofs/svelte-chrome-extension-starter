f9e78cf97c3f7b2a611f26293a976a62fd5aeef6
Andre Roelofs <andreroelofsai@gmail.com>
2024-12-27 18:24:06 +0100

    fix: background now correctly tracks refreshed pages

--- apps/scripts/background/src/lib/background.ts
+++ apps/scripts/background/src/lib/background.ts
@@ -1,30 +1,19 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes('youtube.com/watch')) {
-        const queryParameters = tab.url.split('?')[1].split('&')[0];
-        const urlParameters = new URLSearchParams(queryParameters);
-
-        console.log('Sending message to tab', tabId);
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    // Only run when the tab is done loading,
+    // and make sure it's the right YouTube watch page
+    if (
+        changeInfo.status === 'complete' &&
+        tab.url &&
+        tab.url.includes('youtube.com/watch')
+    ) {
+        // Instead of splitting off only the first parameter,
+        // pass the full query string to URLSearchParams
+        const queryString = tab.url.split('?')[1] || '';
+        const urlParameters = new URLSearchParams(queryString);
 
         chrome.tabs.sendMessage(tabId, {
             type: 'NEW',
             videoId: urlParameters.get('v'),
         });
     }
-});
-
-// Add listener on first load page
-chrome.tabs.onActivated.addListener((activeInfo) => {
-    chrome.tabs.get(activeInfo.tabId, (tab) => {
-        if (tab.url && tab.url.includes('youtube.com/watch')) {
-            const queryParameters = tab.url.split('?')[1].split('&')[0];
-            const urlParameters = new URLSearchParams(queryParameters);
-
-            console.log('Sending message to tab', activeInfo.tabId);
-
-            chrome.tabs.sendMessage(activeInfo.tabId, {
-                type: 'NEW',
-                videoId: urlParameters.get('v'),
-            });
-        }
-    });
 });
